50ec4804b6b533295d24dd6117c6272bf1f5b0dd
nhongming <hongming.ng.2021@scis.smu.edu.sg>
2023-02-25 21:46:41 +0800

    frontend jwt auth spring security config
    frontend login authentication logic

---
+++ client/jwt-auth-login/src/services/user.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/api/test/';
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
+  }
+
+  getUserBoard() {
+    return axios.get(API_URL + 'user', { headers: authHeader() });
+  }
+
+  getVendorBoard() {
+    return axios.get(API_URL + 'vendor', { headers: authHeader() });
+  }
+
+  getAdminBoard() {
+    return axios.get(API_URL + 'admin', { headers: authHeader() });
+  }
+
+  getApproverBoard() {
+    return axios.get(API_URL + 'approver', {headers: authHeader()});
+  }
+}
+
+export default new UserService();
