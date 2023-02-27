9479e2d5088f6258e9ef7d4a0c03d12706cf73ef
ZlKen2 <zllow.2021@smu.edu.sg>
2023-02-27 13:09:50 +0800

    “navbar,routing,palette

--- client/form-builder/src/App.js
+++ client/form-builder/src/App.js
@@ -1,28 +1,70 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route }
   from 'react-router-dom';
-import Home from './pages/index';
+import Home from './pages';
 import AccountManagementPage from './pages/AccountManagementPage';
 import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
-import FormBuilder from './pages/formBuilder';
+import FormBuilder from './pages/FormBuilder';
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path='/Home' element={<Home />} />
-        <Route path='/AccountManagementPage' element={<AccountManagementPage />} />
-        <Route path='/Settings' element={<Settings />} />
-        <Route path='/Dashboard' element={<Dashboard />} />
-        <Route path='/form-builder' element={<FormBuilder />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/Home' element={<Home />} />
+          <Route path='/AccountManagementPage' element={<AccountManagementPage />} />
+          <Route path='/Settings' element={<Settings />} />
+          <Route path='/Dashboard' element={<Dashboard />} />
+          <Route path='/FormBuilder' element={<FormBuilder />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#88CBED',
+      main: '#1F87BC',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      light: '#BCBCBC',
+      main: '#7F7F7F',
+      contrastText: '#FFFFFF',
+
+    },
+
+    success: {
+      light: '#A5DEB8',
+      main: '#70C18C',
+      contrastText: '#FFFFFF',
+    },
+    danger: {
+      light: '#EBB4B4',
+      main: '#EB8C8C',
+      contrastText: '#FFFFFF',
+    },
+    warning: {
+      light: '#F6DEAF',
+      main: '#F4CC7E',
+    },
+    info: {
+      light: '#A7E8F1',
+      main: '#57B9C6',
+      contrastText: '#FFFFFF',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
 
 export default App;
 
