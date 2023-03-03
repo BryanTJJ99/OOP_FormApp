97cf3d51f3985b26fba27b949dc36bb5169645c1
Chan Zhe Xiang <zxchan.2021@scis.smu.edu.sg>
2023-03-03 16:43:32 +0800

    update account creation page

--- client/form-builder/src/components/AccountCreation/AccountCreationField.jsx
+++ client/form-builder/src/components/AccountCreation/AccountCreationField.jsx
@@ -3,12 +3,21 @@ import React from "react";
 import { TextField } from "@mui/material";
 
 const AccountCreationField = (props) => {
+    let accountDetails = props.accountDetails;
+
     return (
         <TextField
             id={props.field}
             label={props.field}
             variant="outlined"
             style={{ marginBottom: "10px" }}
+            inputProps={{
+                autoComplete: "off",
+            }}
+            onChange={(e) => {
+                accountDetails[props.field] = e.target.value;
+                props.setAccountDetails(accountDetails);
+            }}
         />
     );
 };
--- client/form-builder/src/components/AccountCreation/SubmitAccountCreation.jsx
+++ client/form-builder/src/components/AccountCreation/SubmitAccountCreation.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const SubmitAccountCreation = () => {
     return (
-        <Button variant="contained" color="primary">
+        <Button
+            component={Link}
+            to={"/AccountManagementPage"}
+            variant="contained"
+            color="primary"
+            onClick={() => {
+                console.log("submitted");
+                // this function will send the information to the backend to create a new user
+            }}
+        >
             Submit
         </Button>
     );
