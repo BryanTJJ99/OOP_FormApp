5df26d66f6e39e722bb01c90d1894c8ac452c0d4
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2023-03-13 00:41:38 +0800

    added @Service to ProjectServiceImpl.java

--- backend/src/main/java/com/oopproject/form/service/ProjectServiceImpl.java
+++ backend/src/main/java/com/oopproject/form/service/ProjectServiceImpl.java
@@ -3,10 +3,12 @@ package com.oopproject.form.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.oopproject.form.models.Project.Project;
 import com.oopproject.form.repositories.ProjectRepository;
 
+@Service
 public class ProjectServiceImpl implements ProjectService {
 
     @Autowired
