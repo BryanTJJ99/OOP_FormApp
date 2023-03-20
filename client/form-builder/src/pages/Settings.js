e87f50ca2e04c0a947ef18f934fd7aae772cf614
nhongming <hongming.ng.2021@scis.smu.edu.sg>
2023-03-20 17:39:38 +0800

    Update Settings.js to render the payload response forr JWTResponse data

--- client/form-builder/src/pages/Settings.js
+++ client/form-builder/src/pages/Settings.js
@@ -18,6 +18,7 @@ import AuthService from "../services/authService";
 
 const Settings = () => {
     const currentUser = AuthService.getCurrentUser();
+    console.log(currentUser);
     const theme = useTheme();
     const Item = styled(Paper)(({ theme }) => ({
         ...theme.typography.body2,
@@ -74,6 +75,26 @@ const Settings = () => {
                             </Typography>
                             <Typography variant="h6" component="div" gutterBottom>
                                 {currentUser.roles}
+                            </Typography>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Item>
+                            <Typography variant="h6" component="div" gutterBottom>
+                                Access Token
+                            </Typography>
+                            <Typography variant="h6" component="div" gutterBottom>
+                                {currentUser.accessToken}
+                            </Typography>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Item>
+                            <Typography variant="h6" component="div" gutterBottom>
+                                Token Type
+                            </Typography>
+                            <Typography variant="h6" component="div" gutterBottom>
+                                {currentUser.tokenType}
                             </Typography>
                         </Item>
                     </Grid>
