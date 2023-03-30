bcc0a43bf885d16075a2d962ad8a441b12d208d9
LohKenMing <111495971+LohKenMing@users.noreply.github.com>
2023-03-30 14:30:01 +0800

    pdf now generates html string

--- backend/src/main/java/com/oopproject/form/controllers/PdfController.java
+++ backend/src/main/java/com/oopproject/form/controllers/PdfController.java
@@ -1,42 +1,39 @@
 package com.oopproject.form.controllers;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.Optional;
+import java.nio.charset.Charset;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.pdf.PdfWriter;
-import com.lowagie.text.Document;
-import com.oopproject.form.models.PdfFile.PdfFile;
-import com.oopproject.form.repositories.PdfRepository;
-
-// import jakarta.mail.internet.ContentDisposition;
-
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 @RestController
 public class PdfController {
+
     @GetMapping("/generate-pdf")
     public ResponseEntity<byte[]> generatePdf() throws Exception {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Document document = new Document();
-        PdfWriter.getInstance(document, baos);
+        Document document = new Document(PageSize.A4);
+        PdfWriter writer = PdfWriter.getInstance(document, baos);
         document.open();
-        document.add(new Paragraph("Hello, World!"));
+
+        // MAKE THE FORM HERE AND STORE AS A HTML STRING
+        String htmlContent = "<html><body><h1>Hello World</h1><h2>OOP</h2></body></html>";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(htmlContent.getBytes(Charset.forName("UTF-8")));
+
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document, inputStream, Charset.forName("UTF-8"));
+
         document.close();
         byte[] pdfBytes = baos.toByteArray();
         HttpHeaders headers = new HttpHeaders();
